ad59e5f66288ce5e24dbbc11cb7fdc3bbb1a7d13
louam1 <47502570+louam1@users.noreply.github.com>
2021-12-09 09:20:03 -0800

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,10 +1,12 @@
 from __future__ import print_function
 from googleapiclient.discovery import build 
 from google.oauth2 import service_account
+npm install dotenv --save
+require('dotenv').config()
 SCOPES = [
 'https://www.googleapis.com/auth/spreadsheets',
 'https://www.googleapis.com/auth/drive'
 ]
-credentials = service_account.Credentials.from_service_account_file('credentials.json', scopes=SCOPES)
+credentials = service_account.Credentials.from_service_account_file('credentials.env', scopes=SCOPES)
 spreadsheet_service = build('sheets', 'v4', credentials=credentials)
 drive_service = build('drive', 'v3', credentials=credentials)
