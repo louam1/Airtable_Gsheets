35eb4016c9272bf9ce763daa735d8a7bafb5cebd
louam1 <47502570+louam1@users.noreply.github.com>
2021-12-09 13:34:18 -0800

    reverting credentials to json

--- auth.py
+++ auth.py
@@ -1,12 +1,12 @@
 from __future__ import print_function
 from googleapiclient.discovery import build 
 from google.oauth2 import service_account
-from dotenv import load_dotenv
+
 
 SCOPES = [
 'https://www.googleapis.com/auth/spreadsheets',
 'https://www.googleapis.com/auth/drive'
 ]
-credentials = service_account.Credentials.from_service_account_file(load_dotenv('credentials.env'), scopes=SCOPES)
+credentials = service_account.Credentials.from_service_account_file(load_dotenv('credentials.json'), scopes=SCOPES)
 spreadsheet_service = build('sheets', 'v4', credentials=credentials)
 drive_service = build('drive', 'v3', credentials=credentials)
